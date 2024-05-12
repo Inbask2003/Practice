4359873fd4f9c749dfbdad74c1526587d7947674
Santhosh Kumar <inbask2003@gmail.com>
2024-05-12 07:23:40 +0530

    removed sentence

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello
     public static void main(String args[])
     {
         System.out.println("Hello World! Santhosh Kumar");
-        System.out.println("Welcome");
+        
     }
 }
