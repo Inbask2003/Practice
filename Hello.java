6150807d8731ad6ede8cae6904fd8378aa992674
inbask2003 <inbask2003@gmail.com>
2024-05-13 06:36:05 +0530

    Final Global User Checking

--- Hello.java
+++ Hello.java
@@ -2,18 +2,6 @@ public class Hello
 {
     public static void main(String args[])
     {
-        add();
-    }
-    static void add()
-    {
         System.out.println("Hello Class Add");
-    }
-}
-
-class Welcome
-{
-    static void add()
-    {
-        System.out.println("Welcome Class Add");
     }
 }
