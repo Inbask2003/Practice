fc2ab10c805d9bc2f8350cd1c2a7e8ca6e7d23cf
inbask2003 <inbask2003@gmail.com>
2024-05-16 17:05:32 +0530

    Create Interface.java File

---
+++ Interface.java
@@ -0,0 +1,23 @@
+// Interface only have abstract methods. Inteface Variables are static & final implicitly
+
+public class Interface implements Face
+{
+    public static void main(String[] args) 
+    {
+        Interface obj = new Interface();
+        obj.show();   
+        System.out.println(Face.a);
+    }
+
+    public void show()
+    {
+        System.out.println("Hello");
+    }
+    
+}
+
+interface Face
+{
+    int a = 10;
+    public abstract void show();    
+}
